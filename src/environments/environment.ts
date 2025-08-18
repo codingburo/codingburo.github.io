e06b823088ef8a334817e0d1f9ad6ed4bb01697a
SBee <gitbaq@gmail.com>
2025-08-18 13:44:20 +1000

    tracking prod env

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'secrets.FIREBASE_API_KEY',
+    authDomain: 'secrets.FIREBASE_AUTH_DOMAIN',
+    projectId: 'secrets.FIREBASE_PROJECT_ID',
+    storageBucket: 'secrets.FIREBASE_STORAGE_BUCKET',
+    messagingSenderId: 'secrets.FIREBASE_MESSAGING_SENDER_ID',
+    appId: 'secrets.FIREBASE_APP_ID',
+    measurementId: 'secrets.FIREBASE_MEASUREMENT_ID',
+  },
+};
