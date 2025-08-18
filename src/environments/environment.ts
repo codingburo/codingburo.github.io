3d18d1db54bcfcbc7883c90b3ea79af8f3bfcaf5
SBee <gitbaq@gmail.com>
2025-08-18 20:14:46 +1000

    env props

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
 
-  firebase: '${{secrets.FIREBASE_APP}}',
+  firebase: {
+    apiKey: '${{ secrets.FIREBASE_API_KEY }}',
+    authDomain: '${{ secrets.FIREBASE_AUTH_DOMAIN }}',
+    projectId: '${{ secrets.FIREBASE_PROJECT_ID }}',
+    storageBucket: '${{ secrets.FIREBASE_STORAGE_BUCKET }}',
+    messagingSenderId: '${{ secrets.MESSAGING_SENDER_ID }}',
+    appId: '${{ secrets.FIREBASE_APP_ID }}',
+    measurementId: '${{ secrets.FIREBASE_MEASUREMENT_ID }}',
+  },
 };
