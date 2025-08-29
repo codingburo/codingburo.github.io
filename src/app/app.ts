d24fc2f7b80a4698dd3c92e72ba5ae5c7e489b11
SBee <gitbaq@gmail.com>
2025-08-29 16:21:26 +1000

    Chat component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Header } from './components/header/header';
 import { RouterOutlet } from '@angular/router';
-import { Fluid } from 'primeng/fluid';
 import { Toast } from 'primeng/toast';
 import { AuthService } from './services/auth-service';
 import { ConfirmDialog } from 'primeng/confirmdialog';
@@ -10,7 +9,7 @@ import { ConfirmDialog } from 'primeng/confirmdialog';
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Header, RouterOutlet, Fluid, Toast, ConfirmDialog],
+  imports: [Header, RouterOutlet, Toast, ConfirmDialog],
   providers: [],
 })
 export class App implements OnInit {
