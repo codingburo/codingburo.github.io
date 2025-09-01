a7fa1d6e648e166ff5814df15f1123fd31324b4b
SBee <gitbaq@gmail.com>
2025-09-01 10:31:52 +1000

    Brand Style

--- src/app/components/brand/brand.ts
+++ src/app/components/brand/brand.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BrandLogo } from '../brand-logo/brand-logo';
 
 @Component({
   selector: 'app-brand',
-  imports: [RouterLink],
+  imports: [RouterLink, BrandLogo],
   templateUrl: './brand.html',
   styleUrl: './brand.css',
 })
 export class Brand {
+  @Input() showLogo: boolean = true;
   protected readonly title = signal('Cobu');
 }
